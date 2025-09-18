21886b85758366b40881120534d1f4236dde5d80
Chappidi Vishnu <vishnuchappidi12@gmail.com>
2025-09-18 09:45:32 +0000

    intial commit

---
+++ definitions/source.js
@@ -0,0 +1,16 @@
+// definitions/sources/sources.js
+
+declare({
+  schema: "sales_dataset",
+  name: "orders"
+});
+
+declare({
+  schema: "sales_dataset",
+  name: "order_items"
+});
+
+declare({
+  schema: "sales_dataset",
+  name: "payments"
+});
